e548291462121823f2404cf869aa0b5185045494
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2021-11-29 13:05:53 -0600

    org - remove unused files and dirs

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  verbose: true,
-  testURL: 'http://localhost/',
-  collectCoverageFrom: ['modules/*/src/**/*.js', '!**/node_modules/**'],
-  testPathIgnorePatterns: ['/node_modules/', '/website/']
-};
